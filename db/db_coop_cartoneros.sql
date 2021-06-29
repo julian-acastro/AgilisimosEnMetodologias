fcda56b63cb07e3de660dfd6060ae0c9c1fb028a
sergioyanez <sergiomyanez02@gmail.com>
2021-06-29 16:25:02 -0300

    se corrigio db

--- db/db_coop_cartoneros.sql
+++ db/db_coop_cartoneros.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-06-2021 a las 21:15:46
+-- Tiempo de generación: 29-06-2021 a las 21:23:57
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 8.0.6
 
